8ff5bde1591148dc7b22a672fd8a309941dc5a25
jake_xiao <jake_xiao@yeah.net>
2021-08-30 00:53:07 +0800

    1680 problem

---
+++ 1680.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
+func concatenatedBinary(n int) int {
+	primeBase := int(math.Pow(float64(10), float64(9))) + 7
+	result := 0
+	remains := 1
+	move := 0
+	two := 1
+
+	for i := n; i >= 1; i-- {
+		two = int((uint64(math.Pow(float64(2), float64(move)))%uint64(primeBase))*(uint64(two))) % primeBase
+		remains = int((uint64(i) * uint64(two)) % uint64(primeBase))
+		result = (result + remains) % primeBase
+		move = len(strconv.FormatInt(int64(i), 2))
+	}
+	return result
+}
+
+func main() {
+	fmt.Println(concatenatedBinary(100000))
+}
