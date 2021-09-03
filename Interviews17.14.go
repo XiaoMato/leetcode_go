ab4fa04be82a4f0d1eb3f04d3a350acc71c368e8
jake_xiao <jake_xiao@yeah.net>
2021-09-03 23:30:19 +0800

    add problems

--- Interviews17.14.go
+++ Interviews17.14.go
@@ -1,27 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"math"
-	"sort"
 )
-
-// func smallestK(arr []int, k int) []int {
-// 	result := make([]int, k+1)
-// 	for i, _ := range result {
-// 		result[i] = math.MaxInt32
-// 	}
-
-// 	for _, v := range arr {
-// 		result[0] = v
-// 		for i := 1; i < len(result) && result[i] > result[i-1]; i++ {
-// 			temp := result[i]
-// 			result[i] = result[i-1]
-// 			result[i-1] = temp
-// 		}
-// 	}
-// 	return result[1:]
-// }
 
 func smallestK(arr []int, k int) []int {
 	result := make([]int, k+1)
@@ -38,10 +19,4 @@ func smallestK(arr []int, k int) []int {
 		}
 	}
 	return result[1:]
-	sort.Ints(arr)
-	return arr[:k]
-}
-
-func main() {
-	fmt.Println(smallestK([]int{1, 3, 5, 7, 2, 4, 6, 8}, 4))
 }
