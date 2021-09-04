86f064990835a39e95d012200070624d74dce302
jake_xiao <jake_xiao@yeah.net>
2021-09-04 22:23:56 +0800

    add problems

--- CodingInterviews10.go
+++ CodingInterviews10.go
@@ -1,21 +1,15 @@
 package main
 
 func fib(n int) int {
-	if n == 0 {
-		return 0
-	}
-
-	if n == 1 {
-		return 1
+	if n < 2 {
+		return n
 	}
 
 	first, second, third := 0, 1, 1
-
 	for i := 2; i <= n; i++ {
 		third = first + second
 		first = second
 		second = third
 	}
-
 	return second
 }
